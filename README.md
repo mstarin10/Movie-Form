bad39db1c6ee050463d072a698f00afc011567fe
mstarin10 <58877875+mstarin10@users.noreply.github.com>
2019-12-20 22:43:06 -0500

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# Movie-Form
-Created with CodeSandbox
---
+++ src/app/hero-form/hero-form.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+
+import { Hero }    from '../hero';
+
+@Component({
+  selector: 'app-hero-form',
+  templateUrl: './hero-form.component.html',
+  styleUrls: ['./hero-form.component.css']
+})
+export class HeroFormComponent {
+
+  powers = ['Just Released', 'This Year',
+            'This Decade', 'Old as Balls'];
+
+  model = new Hero(18, '8===>', this.powers[0], 'Enter Movie Here');
+
+  submitted = false;
+
+  onSubmit() { this.submitted = true; }
+
+  // TODO: Remove this when we're done
+  get diagnostic() { return JSON.stringify(this.model); }
+
+  newHero() {
+    this.model = new Hero(42, '', '');
+  }
+
+  skyDog(): Hero {
+    let myHero =  new Hero(42, 'SkyDog',
+                           'Fetch any object at any distance',
+                           'Leslie Rollover');
+    console.log('My hero is called ' + myHero.name); // "My hero is called SkyDog"
+    return myHero;
+  }
+
+  //////// NOT SHOWN IN DOCS ////////
+
+  // Reveal in html:
+  //   Name via form.controls = {{showFormControls(heroForm)}}
+  showFormControls(form: any) {
+    return form && form.controls['name'] &&
+    form.controls['name'].value; // Dr. IQ
+  }
+
+  /////////////////////////////
+
+}
